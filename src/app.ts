d97a0030c08a96230aa962dc6789f70726a8a448
Sudipta Das <shiningsudipto@gmail.com>
2024-11-11 09:12:45 +0600

    folder structure and route setup done

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import router from "./app/routes";
+import httpStatus from "http-status";
 
 const app: Application = express();
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
-    Message: "Ph health care server..",
+    Message: "Library management server...",
+  });
+});
+
+app.use("/api", router);
+
+app.use((req: Request, res: Response) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
   });
 });
 
